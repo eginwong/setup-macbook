0098e27217bbaab9c6091c8abc9bab78727f36a9
eginwong <e.gin.wong@gmail.com>
2018-06-02 21:03:08 -0400

    feat: vscode extension and formatting

--- setup-mbp-2018.sh
+++ setup-mbp-2018.sh
@@ -12,44 +12,35 @@
 
 # METHODS
 create_dev_folder() {
-if [ ! -d "dev" ]; then
-  echo "LOG: create dev folder"
-  mkdir dev
-fi
+  if [ ! -d "dev" ]; then
+    echo "LOG: create dev folder"
+    mkdir dev
+  fi
 }
 
 setup_alias() {
-ALIASLL="alias ll='ls -Al'"
-
-if [[ -e ~/.bash_profile ]] && grep -qF "$ALIASLL" ~/.bash_profile;then
-  echo "alias already present"
-else
-  echo $ALIASLL >> ~/.bash_profile
-fi
-
-. ~/.bash_profile
+  ALIASLL="alias ll='ls -Al'"
+  
+  if [[ -e ~/.bash_profile ]] && grep -qF "$ALIASLL" ~/.bash_profile;then
+    echo "alias already present"
+  else
+    echo $ALIASLL >> ~/.bash_profile
+  fi
+  
+  . ~/.bash_profile
 }
 
 homebrew() {
-if which brew | grep -q 'no brew' || [[ $(which brew | head -c1 | wc -c) -eq 0 ]]; then
-  echo "HOMEBREW:: not installed"
-  # From the website: https://brew.sh/ -20180602
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  homebrew
-else
-  echo "HOMEBREW:: installed"
-fi
+  if which brew | grep -q 'no brew' || [[ $(which brew | head -c1 | wc -c) -eq 0 ]]; then
+    echo "HOMEBREW:: not installed"
+    # From the website: https://brew.sh/ -20180602
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    homebrew
+  else
+    echo "HOMEBREW:: installed"
+  fi
 }
 
-vscode() {
-if which code | grep -q 'no code' || [[ $(which code | head -c1 | wc -c) -eq 0 ]]; then
-  echo "VSCODE:: not installed"
-  brew install homebrew/cask/visual-studio-code
-  vscode
-else
-  echo "VSCODE:: installed"
-fi
-}
 
 ruby_update() {
   echo "RUBY:: currently at $(ruby -v)"
@@ -58,31 +49,43 @@ ruby_update() {
   echo "RUBY:: updated"
 }
 
+vscode() {
+  if which code | grep -q 'no code' || [[ $(which code | head -c1 | wc -c) -eq 0 ]]; then
+    echo "VSCODE:: not installed"
+    brew install homebrew/cask/visual-studio-code
+    vscode
+  else
+    echo "VSCODE:: installed"
+  fi
+}
+
+code_extensions() {
+  if [[ $(code --list-extensions | grep 'shakram02.bash-beautify' | head -c1 | wc -c) -eq 0 ]]; then
+    code --install-extension shakram02.bash-beautify
+    code_extensions
+  else
+    echo "VSCODE:: extensions installed"
+  fi
+}
 
 main() {
-
-# create main working folder
-cd ~ || exit
-
-create_dev_folder
-
-# enter main working folder
-cd dev || exit
-
-setup_alias
-
-homebrew
-ruby_update
-vscode
-
-# return to home
-cd ~
-
+  # create main working folder
+  cd ~ || exit
+  
+  create_dev_folder
+  
+  # enter main working folder
+  cd dev || exit
+  
+  setup_alias
+  
+  homebrew
+  ruby_update
+  vscode
+  code_extensions
+  
+  # return to home
+  cd ~
 }
 
 main
-
-# LOG PROGRAMS TO INSTALL
-# TRY TO GET BREW INSTALL TO WORK visual-studio-code
-# GIT
-# VSCODE: https://code.visualstudio.com/download
